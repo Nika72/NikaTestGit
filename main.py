53f75d982b1015527fe9c089664e76132f84c8f1
Nika Chavleishvili <s25686@pjwstk.edu.pl>
2024-03-23 18:36:20 +0100

    this is my fourth commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def print_hi(name):
-    print(f'Hii2, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hii3, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
